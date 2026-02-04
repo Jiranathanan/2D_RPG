2c8b44c9eb660254b6362d3f98f8a3aa8609a72d
Jiranathanan Siripun <jiranathanan@gmail.com>
2026-02-04 13:00:21 +0700

    Player State Machine

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -1,12 +1,34 @@
 extends KinematicBody2D
 
+enum PLAYER_STATE { IDLE, RUN}
+
 export(float) var move_speed = 100
 onready var sprite = $Sprite
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree.get("parameters/playback")
+
+var current_state = PLAYER_STATE.IDLE setget set_current_state
+
+func set_current_state(new_state):
+	if(current_state != new_state):
+		match(new_state):
+			PLAYER_STATE.IDLE:
+				state_machine.travel("idle")
+			PLAYER_STATE.RUN:
+				state_machine.travel("run")
+			
+	current_state = new_state
 
 func _physics_process(delta):
 	var input_xy = get_player_movement_input()
 	
+	# Picking the new state for the player
 	var velocity = move_and_slide(input_xy * move_speed)
+	if(velocity == Vector2.ZERO):
+		self.current_state = PLAYER_STATE.IDLE	
+	else:
+		self.current_state = PLAYER_STATE.RUN
+	
 	
 	set_flip_direction(velocity)
 	
